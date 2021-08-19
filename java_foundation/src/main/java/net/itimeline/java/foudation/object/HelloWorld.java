7d80714c7867f7646685c4568ca2acc00856b54d
mengfenguang <2604939074@qq.com>
2021-08-19 10:24:07 +0800

    我的第一个java程序

---
+++ java_foundation/src/main/java/net/itimeline/java/foudation/object/HelloWorld.java
@@ -0,0 +1,16 @@
+package net.itimeline.java.foudation.object;/**
+ * @author mfg
+ * @version 1.0
+ */
+
+/**
+ *@author mengfengguang
+ *@since jdk8--me
+ *@version 2021/8/19 10:18
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
+
